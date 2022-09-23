0bc1345a5ba22b249d03808d860fcec333a7c671
Faiz Ahnaf <93417786+fzhnf@users.noreply.github.com>
2022-09-23 16:46:39 +0800

    Create Konversi celcius to Fahrenheit.py

---
+++ Konversi celcius to Fahrenheit.py
@@ -0,0 +1,14 @@
+# Program python untuk konversi Celcius ke Fahrenheit
+
+# ubah nilai untuk hasil yang berbeda
+celcius = float(input("Masukkan Suhu Celcius = "))
+
+# menghitung fahrenheit
+fahrenheit = celcius * 1.8 + 32
+    
+#menampilkan hasil perhitungan
+print(f"""
+Konversi Suhu Celcius menjadi Suhu Fahrenheit
+= ({celcius}°C * 1.8) + 32
+= {fahrenheit}°f
+""")
